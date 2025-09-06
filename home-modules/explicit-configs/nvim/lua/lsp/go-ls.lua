3e32d501afd2cb4e92c215273223cc4a2c3b19ab
jmug <u.g.a.mariano@gmail.com>
2025-09-05 18:53:30 -0700

    Don't use placeholders with gopls.

--- home-modules/explicit-configs/nvim/lua/lsp/go-ls.lua
+++ home-modules/explicit-configs/nvim/lua/lsp/go-ls.lua
@@ -25,7 +25,7 @@ lspconfig.gopls.setup {
   },
   autostart = true,
   root_dir = lspconfig.util.root_pattern(".git","go.mod"),
-  init_options = {usePlaceholders = true, completeUnimported = true},
+  init_options = {usePlaceholders = false, completeUnimported = true},
   -- capabilities = capabilities,
   single_file_support = true;
 }
